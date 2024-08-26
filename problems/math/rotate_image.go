b16784dd23152ed00832db37eb2889e2fbeae3ac
anirudhology <anirudhology@gmail.com>
2024-08-26 07:23:24 +0530

    Add problem 48 - Rotate Image

---
+++ problems/math/rotate_image.go
@@ -0,0 +1,22 @@
+package math
+
+func RotateImage(matrix [][]int) {
+	// Special case
+	if len(matrix) == 0 {
+		return
+	}
+	// Order of the matrix
+	n := len(matrix)
+	// Left and right pointers
+	left, right := 0, n-1
+	for left < right {
+		matrix[left], matrix[right] = matrix[right], matrix[left]
+		left++
+		right--
+	}
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+}
---
+++ tests/math/rotate_image_test.go
@@ -0,0 +1,67 @@
+package math_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/anirudhology/leetcode-go/problems/math"
+)
+
+func TestRotate(t *testing.T) {
+	// Test case 1: 3x3 matrix
+	matrix1 := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+	expected1 := [][]int{
+		{7, 4, 1},
+		{8, 5, 2},
+		{9, 6, 3},
+	}
+	math.RotateImage(matrix1)
+	if !reflect.DeepEqual(matrix1, expected1) {
+		t.Errorf("Expected %v, but got %v", expected1, matrix1)
+	}
+
+	// Test case 2: 4x4 matrix
+	matrix2 := [][]int{
+		{5, 1, 9, 11},
+		{2, 4, 8, 10},
+		{13, 3, 6, 7},
+		{15, 14, 12, 16},
+	}
+	expected2 := [][]int{
+		{15, 13, 2, 5},
+		{14, 3, 4, 1},
+		{12, 6, 8, 9},
+		{16, 7, 10, 11},
+	}
+	math.RotateImage(matrix2)
+	if !reflect.DeepEqual(matrix2, expected2) {
+		t.Errorf("Expected %v, but got %v", expected2, matrix2)
+	}
+
+	// Test case 3: 1x1 matrix
+	matrix3 := [][]int{{1}}
+	expected3 := [][]int{{1}}
+	math.RotateImage(matrix3)
+	if !reflect.DeepEqual(matrix3, expected3) {
+		t.Errorf("Expected %v, but got %v", expected3, matrix3)
+	}
+
+	// Test case 4: Empty matrix
+	matrix4 := [][]int{}
+	expected4 := [][]int{}
+	math.RotateImage(matrix4)
+	if !reflect.DeepEqual(matrix4, expected4) {
+		t.Errorf("Expected %v, but got %v", expected4, matrix4)
+	}
+
+	// Test case 5: Nil matrix
+	var matrix5 [][]int = nil
+	math.RotateImage(matrix5)
+	if matrix5 != nil {
+		t.Errorf("Expected nil, but got %v", matrix5)
+	}
+}
